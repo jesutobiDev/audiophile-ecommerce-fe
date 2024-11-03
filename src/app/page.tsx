9197be154bc18203fe73d0dfb12dd52bb58cc1c8
Bada Jesutobi <jesutobi_dev@outlook.com>
2024-11-03 12:52:29 +0100

    styles:header responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,13 @@ const HomePage = () => {
   return (
     <>
       <Header>
-        <div className="flex justify-between items-center py-7">
-          <div className="w-full md:w-[45%] space-y-4">
-            <p className="uppercase opacity-50 tracking-widest font-light text-sm">New Product</p>
+        <div className="flex lg:justify-between justify-center items-center py-7 relative px-0 lg:px-28">
+          <div className="w-full md:w-[70%] lg:w-[45%] space-y-4 absolute lg:static z-10 flex flex-col items-center justify-center lg:items-start px-10 lg:px-0 text-center lg:text-left">
+            <p className="uppercase opacity-50 tracking-widest font-light text-lg md:text-xl lg:text-sm">New Product</p>
             <h1 className="text-5xl font-bold uppercase">XX99 Mark II Headphones</h1>
-            <p className="opacity-75 w-10/12">Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
+            <p className="opacity-75 w-10/12 md:text-lg lg:text-base">Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
           </div>
-          <div className="w-full md:w-[55%] relative h-[430px]">
+          <div className="w-full lg:w-[55%] relative h-[430px] md:h-[500px] lg:h-[430px]">
             <Image 
               src="https://res.cloudinary.com/dxzq8zubp/image/upload/v1730629756/headphone_rgafzt.png" 
               alt="XX99 Mark II Headphones" 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,36 +1,79 @@
-import Link from "next/link";
+"use client";
 import Image from "next/image";
+import { useState, useEffect } from "react";
+import { useRouter, usePathname } from "next/navigation";
+
+interface NavLink {
+  text: string;
+  href: string;
+  active: boolean;
+}
 
 interface HeaderProps {
   children?: React.ReactNode;
 }
 
 const Header: React.FC<HeaderProps> = ({ children }) => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const [navLinks, setNavLinks] = useState<NavLink[]>([
+    { text: "Home", href: "/", active: false },
+    { text: "Headphones", href: "/headphones", active: false },
+    { text: "Speakers", href: "/speakers", active: false },
+    { text: "Earphones", href: "/earphones", active: false },
+  ]);
+
+  useEffect(() => {
+    setNavLinks(prevLinks =>
+      prevLinks.map(link => ({
+        ...link,
+        active: link.href === pathname,
+      }))
+    );
+  }, [pathname]);
+
+  const handleLinkClick = (href: string) => {
+    router.push(href);
+  };
+
   return (
-    <header className="bg-black text-white px-28">
-      <nav className="py-7 flex justify-between items-center border-b border-white/30">
-        <Link href="/">
+    <header className="bg-black text-white">
+      <nav className="py-7 flex justify-between items-center border-b border-white/30 px-10 lg:px-0 lg:mx-28">
+        <div className="lg:hidden">
           <Image
-            src="https://res.cloudinary.com/dxzq8zubp/image/upload/v1730628326/audiophile_2_jqb5fx.svg"
-            alt="audiophile logo"
-            height={25}
-            width={143}
-          />
-        </Link>
-        <div className="uppercase flex items-center gap-10 text-sm font-medium">
-          <Link href="/">Home</Link>
-          <Link href="/headphones">Headphones</Link>
-          <Link href="/speakers">Speakers</Link>
-          <Link href="/earphones">Earphones</Link>
-        </div>
-        <Link href="/cart">
-          <Image
-            src="https://res.cloudinary.com/dxzq8zubp/image/upload/v1730628727/cart_fm8tja.svg"
-            alt="cart logo"
+            src="https://res.cloudinary.com/dxzq8zubp/image/upload/v1730631671/menu_kuoaxt.svg"
+            alt="menu icon"
             height={25}
             width={25}
           />
-        </Link>
+        </div>
+        <Image
+          src="https://res.cloudinary.com/dxzq8zubp/image/upload/v1730628326/audiophile_2_jqb5fx.svg"
+          alt="audiophile logo"
+          height={25}
+          width={143}
+        />
+        <ul className="uppercase lg:flex items-center gap-10 text-sm font-medium hidden tracking-wide">
+          {navLinks.map(link => (
+            <li
+              key={link.text}
+              onClick={() => handleLinkClick(link.href)}
+              className={`cursor-pointer ${
+                link.active ? "text-primary" : "text-white"
+              }`}
+            >
+              {link.text}
+            </li>
+          ))}
+        </ul>
+        <div>
+          <Image
+            src="https://res.cloudinary.com/dxzq8zubp/image/upload/v1730628727/cart_fm8tja.svg"
+            alt="cart icon"
+            height={25}
+            width={25}
+          />
+        </div>
       </nav>
       {children}
     </header>
